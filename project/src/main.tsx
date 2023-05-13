e140162a0368b491f12ed5b337ac9b7e748dd104
Shystra <jlsp.2019@hotmail.com>
2023-05-13 15:46:48 -0300

    learn about routers

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/global.css';
-import { Home } from './components/Home/index.tsx'
+import { Home } from './components/Home/index.tsx';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { About } from './components/About/index.tsx';
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+
+
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />}/>
+
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
